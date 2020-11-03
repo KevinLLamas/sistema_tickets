87053d9b7cda094ba9bc0019e36d9e379f39bbbd
Pachekoko <55992657+Pachekoko@users.noreply.github.com>
2020-11-02 22:21:04 -0600

    mas detallitos, ya quedo

--- resources/views/reportes.blade.php
+++ resources/views/reportes.blade.php
@@ -79,7 +79,7 @@
                   <div class="card shadow mb-4">
                       <!-- Card Header - Dropdown -->
                       <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                          <h6 class="m-0 font-weight-bold text-primary">Reportes : {{Session::get('rol')}}</h6>
+                          <h6 class="m-0 font-weight-bold text-primary">Reporte de Tickets Creados vs Tickets Cerrados : {{Session::get('rol')}}</h6>
                           <div class="dropdown no-arrow">
                               <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                   <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
